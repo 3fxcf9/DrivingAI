7c21fe547b3e01ff9efb0dba4645692e552ccb29
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-07-11 10:00:56 +0200

    Add vector.ts

---
+++ src/vector.ts
@@ -0,0 +1,46 @@
+export class Vector2 {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	copy() {
+		return new Vector2(this.x, this.y);
+	}
+
+	add(vector: Vector2) {
+		this.x += vector.x;
+		this.y += vector.y;
+		return this;
+	}
+
+	substract(vector: Vector2) {
+		this.x -= vector.x;
+		this.y -= vector.y;
+		return this;
+	}
+
+	multiply(scalar: number) {
+		this.x *= scalar;
+		this.y *= scalar;
+		return this;
+	}
+
+	get magnitude() {
+		return Math.sqrt(this.x ** 2 + this.y ** 2);
+	}
+
+	normalize() {
+		this.multiply(1 / this.magnitude);
+		return this;
+	}
+
+	get normalized() {
+		const copy = this.copy();
+		copy.normalize();
+		return copy;
+	}
+}
